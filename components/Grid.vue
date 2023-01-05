b215ae8db987bc12e7907a28a26b4f208fd2d230
Joshua Paino <joshua.paino@gmail.com>
2023-01-05 12:24:38 -0600

    Remove assets toVite

--- components/Grid.vue
+++ components/Grid.vue
@@ -2,7 +2,7 @@
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 px-4 gap-8 2xl:px-0">
         <template v-for="proj in Projects" :key="proj.id">
             <div class="group">
-                <img :src="`/img/${proj.image}.png`" class="rounded-lg cursor-pointer object-cover h-12 h-120 w-full transform-gpu transition-all duration-300 ease-in-out will-change-transform group-hover:scale-102 group-hover:group-odd:rotate-2 group-hover:group-even:-rotate-2" :alt="proj.alt" width="200" height="480" />
+                <img :src="`~/assets/img/${proj.image}.png`" class="rounded-lg cursor-pointer object-cover h-12 h-120 w-full transform-gpu transition-all duration-300 ease-in-out will-change-transform group-hover:scale-102 group-hover:group-odd:rotate-2 group-hover:group-even:-rotate-2" :alt="proj.alt" width="200" height="480" />
             <div class="flex flex-col py-2">
                 <h2 class="text-lg w-full text-gray-800-inline">
                     {{proj.name}}
--- components/Profile.vue
+++ components/Profile.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="felx flex-col mx-auto w-full px-4 gap-4 items-center justify-center md:px-0 md:w-3/4s">
-    <img src="/img/cnn.png" width="120" height="120" class="rounded-full object-cover w-38 h-38 mx-auto" />
+    <img src="~/assets/img/cnn.png" width="120" height="120" class="rounded-full object-cover w-38 h-38 mx-auto" />
     <h1 class="text-center w-full text-4xl text-gray-800/60 italic">
         "Stay hungry, stay foolish!"
     </h1>
